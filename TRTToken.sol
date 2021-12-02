dd20f5aadb271500c534603dc3ebd87fdb23d825
Lincoli Xavier <hello@lincolixavier.com>
2021-12-02 17:16:46 -0300

    feat: add controls to mint and burn tokens

--- TRTToken.sol
+++ TRTToken.sol
@@ -5,7 +5,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 // Simple ERC20 contract, we prefer to leave the rest of the funcionality in other contracts
 contract TRTTOKEN is ERC20 {
+   address public admin;
+
     constructor() ERC20("Treta Coin", "TRT") {
-        _mint(msg.sender, 1000000_000 * 10**decimals());
+        _mint(msg.sender, 10000 * 10 ** decimals());
+        admin = msg.sender;
+    }
+
+    function mint(address to uint amount) external{
+        require(msg.sender == admin, 'only admin');
+        _mint(to, amount)
+    }
+
+    function burn(uint amount) external{
+        _burn(msg.sender, amount)
     }
 }
